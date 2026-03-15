5cfeab4399146e71c06d0a1739ac6b7e0fd7f293
Vaibhav Malkoti <177619278+Va1bhavMalk0t1@users.noreply.github.com>
2026-03-15 12:55:39 +0530

    authMiddlewar created

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,28 @@
-const jwt = require('jsonwebtoken') ; 
+const jwt = require("jsonwebtoken") ; 
+const dotenv = require("dotenv") ; 
+dotenv.config() ; 
+
+const middleWare = (req,res,next)=>{
+
+    const authHeader = req.headers.authorization ; 
+    if(!authHeader){
+    return res.status(401).json({
+        message: "Access denied"
+    });
+    }
+    const token = authHeader.split(" ")[1] ; 
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET) ; 
+        req.user = decoded ; 
+        next() ;
+    }catch(e){
+        return res.status(401).json({
+            message : "Invalid or expired token" 
+        })
+    }
+}
+
+module.exports = {
+    authMiddleware : middleWare 
+}
