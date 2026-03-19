8da6b05a544b439cca6bdf5657f07352f1ff1417
Vaibhav Malkoti <177619278+Va1bhavMalk0t1@users.noreply.github.com>
2026-03-19 18:18:02 +0530

    Added ownerMakerController

---
+++ backend/middleware/adminMiddleware.js
@@ -0,0 +1,42 @@
+const connection = require('../config/db') ; 
+
+const adminMiddleware = (req,res,next) =>{
+    
+    if (!req.user || !req.user.id) {
+    return res.status(401).json({
+        success: false,
+        message: "Unauthorized"
+    });
+    }
+    const id = req.user.id ; 
+    const qry = "select id , role from users where id = ?" ; 
+    connection.query(qry,[id],(err,result)=>{
+        if (err) {
+            return res.status(500).json({
+                success: false,
+                message: "Server error"
+            });
+        }
+
+        if (result.length === 0) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            });
+        }
+
+        if(result[0].role !== 'admin'){
+            return res.status(403).json({
+                success : false , 
+                message : "Access Denied. Admin Only"
+            })
+        }
+
+        req.user = result[0] ; 
+        next() ; 
+    })
+}
+
+module.exports = {
+    adminMiddleware
+}
