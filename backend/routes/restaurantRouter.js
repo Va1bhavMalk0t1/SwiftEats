865c0ff65774a3fef5069e094d79b4e3dd3f378d
Vaibhav Malkoti <177619278+Va1bhavMalk0t1@users.noreply.github.com>
2026-03-20 19:05:03 +0530

    added updateRestaurant route

--- backend/routes/restaurantRouter.js
+++ backend/routes/restaurantRouter.js
@@ -2,7 +2,8 @@ const express = require('express') ;
 const router = express.Router() ; 
 
 const { authMiddleware } = require('../middleware/authMiddleware') ; 
-const { restaurantListController , restaurantSpecificController , restaurantMenuController , createRestaurant , deleteRestaurant } = require('../controllers/restaurantController') ; 
+const { restaurantListController , restaurantSpecificController , restaurantMenuController , createRestaurant , deleteRestaurant
+    , updateRestaurant  } = require('../controllers/restaurantController') ; 
 const {isOwner} = require('../middleware/ownerMiddleware') ; 
 
 router.get('/', authMiddleware ,  restaurantListController) ;
@@ -11,6 +12,7 @@ router.get('/:id/menu',authMiddleware, restaurantMenuController) ;
 
 router.put('/' , authMiddleware , isOwner , createRestaurant ) ; 
 router.delete('/:id' , authMiddleware , isOwner , deleteRestaurant) ; 
+router.update('/:id',authMiddleware,isOwner,updateRestaurant); 
 
 module.exports = {
     router 
