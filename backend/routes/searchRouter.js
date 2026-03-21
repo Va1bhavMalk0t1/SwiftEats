b711bb2d925cd9d4b4ea8e748d7e113e73baaa7f
Vaibhav Malkoti <177619278+Va1bhavMalk0t1@users.noreply.github.com>
2026-03-21 19:23:32 +0530

    added search route

---
+++ backend/routes/searchRouter.js
@@ -0,0 +1,5 @@
+const express = require('express') ; 
+const router = express.Router() ; 
+const {authMiddleware} = require('../middleware/authMiddleware') ; 
+const {searchController} = require('../controllers/searchController') ; 
+router.get('/' , authMiddleware , searchController ) ; 
